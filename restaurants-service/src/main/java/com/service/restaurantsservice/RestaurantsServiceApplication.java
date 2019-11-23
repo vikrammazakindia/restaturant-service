df9916bf4585b6e69f13b051d73632a360498c25
vikram <vickybhaik16@gmail.com>
2019-11-23 02:20:37 -0800

    intial commit

---
+++ restaurants-service/src/main/java/com/service/restaurantsservice/RestaurantsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.restaurantsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsServiceApplication.class, args);
+	}
+
+}
